28927386d6007128f81dfeb1ba24d6a7f359708b
j-ulienguyen <j-ulienguyen@hotmail.com>
2019-10-22 10:28:10 -0700

    Change flex direction and height

--- styles/MainStyles.js
+++ styles/MainStyles.js
@@ -3,9 +3,11 @@ import {StyleSheet} from 'react-native';
 var styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        width: "100%"
+        width: "100%",
+        height: "100%"
     }
 });
 
