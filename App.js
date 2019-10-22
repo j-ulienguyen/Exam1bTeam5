b03e98526a363b02eba4ad1c9437d4adc7bec573
j-ulienguyen <j-ulienguyen@hotmail.com>
2019-10-21 21:09:31 -0700

    Set up

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles/AppStyles';
+
+// Reminder to create a comps folder
+// Import comps below
+
+
+
+function App(){
+	// UI
+	return (
+		<View style={styles.container}>
+			<Text>Remove this line</Text>
+		</View>
+	)
+}
+
+export default App;
